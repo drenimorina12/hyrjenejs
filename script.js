8c321afeaa6301110e9e600ef4d97b7d6d442015
drenimorina12 <morinadreni85@gmail.com>
2024-11-18 18:41:29 -0800

    test

---
+++ script.js
@@ -0,0 +1,20 @@
+//deklarimi i variables = krijojme nje variabel 
+let testVar;
+
+// incializimi-variables i japim vlere per here te pare
+
+testVar = "m"
+
+//deklarimi + incializimi ne te njejtin rresht
+
+const PI = 3.14; //constant eshte vlere e pandryshueshme
+
+//variablat & datatypes
+
+let emri = "dreni";//string
+let notaMatematike = 1; //number
+let isAdult = false ; //true //boolean
+let x; //undefined
+let nullVariabla = null;
+
+// case sensitive
