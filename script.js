5e657601866fc4296569a453fe70a1fe23d67259
drenimorina12 <morinadreni85@gmail.com>
2024-11-19 18:43:45 -0800

    js - vazhdim

--- script.js
+++ script.js
@@ -11,10 +11,42 @@ const PI = 3.14; //constant eshte vlere e pandryshueshme
 
 //variablat & datatypes
 
-let emri = "dreni";//string
-let notaMatematike = 1; //number
-let isAdult = false ; //true //boolean
-let x; //undefined
-let nullVariabla = null;
+// let emri = "dreni";//string
+// let notaMatematike = 1; //number
+// let isAdult = false ; //true //boolean
+// let x; //undefined
+// let nullVariabla = null;
 
 // case sensitive
+
+alert("shfaq nje mesazh");
+
+let eshteMbi7 = confirm("A i ki mbi 7 vjet?");
+console.log(eshteMbi7);
+
+let emri = prompt ("shkruaj emrin");
+alert("Hello" + emri);
+
+let nr1 = 4;
+let nr2 = 3;
+
+let shuma = nr1 + nr2 ;
+let ndryshimi = nr1 - nr2;
+let shumezimi = nr1 * nr2;
+let pjestimi = nr1 / nr2;
+
+console.log("Shuma mes" + nr1 + "dhe" + nr2 + "eshte" + shuma )
+console.log("Zbritja mes" + nr1 + "dhe" + nr2 + "eshte" + ndryshimi )
+console.log(
+    "Shumezimi mes" + nr1 + "dhe" + nr2 + "eshte" + shumezimi )
+console.log(
+    "Pjestimi mes" + nr1 + "dhe" + nr2 + "eshte" + pjestimi )
+
+    let ngritjaNeFuqi = 4 **2;
+
+    let modulo = 16 % 3;
+
+    let numri1 = +prompt ("shkruaj numrin e pare")
+    let numri2 = +prompt ("shkruaj numrin e dyte")
+
+    console.log(numri1 + numri2);
